c0d8ad5692ed0510a92e60aa0994e0547dcf89f6
Ørjan Polden <oerjanbpolden@gmail.com>
2022-05-05 15:52:47 +0200

    refactor anim on load

--- src/components/pages/FadeOnLoad/FadeOnLoadPage.tsx
+++ src/components/pages/FadeOnLoad/FadeOnLoadPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import { SquareBlock } from '@/components/shared/Block/SquareBlock';
+import { AnimFadeOnLoad } from '@/components/shared/Animations/AnimFadeOnLoad';
 
-const Container = styled.div`
+const Container = styled(motion.div)`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -14,8 +16,10 @@ export const FadeOnLoadPage = () => {
   const mapToBlocks = Array.from(Array(10).keys());
   return (
     <Container>
-      {mapToBlocks.map(() => (
-        <SquareBlock />
+      {mapToBlocks.map((_, i) => (
+        <AnimFadeOnLoad key={i}>
+          <SquareBlock />
+        </AnimFadeOnLoad>
       ))}
     </Container>
   );
--- src/components/shared/Animations/AnimFadeOnLoad.tsx
+++ src/components/shared/Animations/AnimFadeOnLoad.tsx
@@ -1,12 +1,26 @@
 import React, { PropsWithChildren } from 'react';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
+
+const loadAnim: Variants = {
+  hide: {
+    opacity: 0,
+    y: 20
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.2
+    }
+  }
+}
 
 export const AnimFadeOnLoad = ({ children }: PropsWithChildren<{}>) => (
   <motion.div
-    initial={{ opacity: 0, y: 10 }}
-    animate={{ opacity: 1, y: 0 }}
-    exit={{ opacity: 0 }}
+    variants={loadAnim}
+    initial="hide"
+    animate="show"
   >
     {children}
-  </motion.div>
+  </motion.div >
 );
